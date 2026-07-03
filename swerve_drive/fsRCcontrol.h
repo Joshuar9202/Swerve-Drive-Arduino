44a42e9eea749248b185b79d6fc929d142b6737c
Joshuar9202 <121123477+Joshuar9202@users.noreply.github.com>
2026-07-02 19:41:34 -0500

    Delete swerve_drive directory

--- swerve_drive/fsRCcontrol.h
+++
@@ -1,8 +0,0 @@
-#ifndef FSRCCONTROL_H
-#define FSRCCONTROL_H
-
-// Declare your functions here
-void setupFSRCcontrol();
-float getFSRCData(int channel);
-
-#endif
--- swerve_drive/gyro.h
+++
@@ -1,9 +0,0 @@
-#ifndef GYRO_H
-#define GYRO_H
-
-// Declare your functions here
-void setupGyro();
-
-float getYaw();
-
-#endif
